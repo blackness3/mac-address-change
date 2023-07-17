af5410c23cd91d12406ece1784464be6563c0c3e
皮卡皮卡 <57695239+heaid@users.noreply.github.com>
2023-07-17 09:38:51 +0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,6 +1,6 @@
 mac="00:"`echo $RANDOM | md5sum | sed 's/\(..\)/&:/g' | cut -c1-14`
 echo $mac
-sudo ifconfig ens16 down
-sudo ifconfig ens16 hw ether $mac
-sudo ifconfig ens16 up
+sudo ifconfig eth0 down
+sudo ifconfig eth0 hw ether $mac
+sudo ifconfig eth0 up
 sudo dhclient &
